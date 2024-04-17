4024dbe201e84b306c7f87e2c096acf4f95a6fbb
scarllat2005 <162135041+scarllat2005@users.noreply.github.com>
2024-04-17 11:30:05 -0300

    Add files via upload

---
+++ desafio2.js
@@ -0,0 +1,10 @@
+const notas = [10,6,8];
+
+notas.push(7);
+
+const  media = (notas[0] + notas[1] + notas[2] + notas[3])/4;
+
+console.log(medida)
+
+
+console.log(notas[0],nota[1],notas[2],notas[3])
