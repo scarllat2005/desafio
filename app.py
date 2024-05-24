d49f4a2a5f938c1c07e408f4bf5ad8b6531723e5
scarllat2005 <162135041+scarllat2005@users.noreply.github.com>
2024-05-24 11:22:54 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+from Classes.restaurante import Restaurante
+
+restaurante_praca = Restaurante('praça', 'Gourmet')
+restaurante_mexicano = Restaurante('Mexican Food', 'Mexicana')
+restaurante_japones = Restaurante('Japa', 'Japonesa')
+
+restaurante_mexicano.alternar_estado()
+
+
+def main():
+    Restaurante.listar_restaurantes()
+
+if __name__ == '__main__':
+    main()
